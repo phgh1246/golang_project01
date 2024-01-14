e8ee9c1e8acaf164f08acdedd8ec90f9712e6839
SC A <sc185@proton.me>
2024-01-13 20:56:15 -0700

    Adding gitignore, makefile, and readme. Also adding basic routing.

---
+++ README.md
@@ -0,0 +1,27 @@
+# Hotel reservation backend
+
+## Dependency modules
+### MongoDB
+Docs
+```
+https://mongodb.com/docs/drivers/go/current/quick-start
+```
+
+
+Install
+```
+go get go.mongodb.org/mongo-driver/mongo
+```
+
+
+### gofiber
+Docs
+```
+https://gofiber.io
+```
+
+
+Install
+```
+go get github.com/gofiber/fiber/v2
+```
--- main.go
+++ main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+  "flag"
   "github.com/gofiber/fiber/v2"
 )
 
 func main() {
+  listenAddr := flag.String("listenAddr", ":5000", "The listen address of the API server")
+  flag.Parse()
+
   app := fiber.New()
+  apiv1 := app.Group("/api/v1")
+
   app.Get("/foo", handleFoo)
-  app.Listen(":5000")
+  apiv1.Get("/user", handleUser)
+  app.Listen(*listenAddr)
 }
 
 func handleFoo(c *fiber.Ctx) error {
   return c.JSON(map[string]string{"msg": "working just fine!"})
+}
+
+func handleUser (c *fiber.Ctx) error {
+  return c.JSON(map[string]string{"user": "James Foo"})
 }
